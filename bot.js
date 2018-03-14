395ecce0a7e78218c84aa89f19cecbe04d9b7f99
Al Vincent Valdez <alvin.valdez.1111@gmail.com>
2018-03-13 23:46:17 -0400

    commit

--- bot.js
+++ bot.js
@@ -10,8 +10,6 @@ client.on('message', message => {
     	message.reply('https://media.giphy.com/media/CNJZ5EYc5pPK8/giphy.gif');
   	}else if(message.content === '?jail'){
 		message.reply('https://media.giphy.com/media/26BRqr52AQWxuUkda/giphy.gif');
-	}else if(message.content === '?byebye'){
-		message.reply(https://media1.tenor.com/images/1e54feb672f69e25892fb3cf3b1bb2d5/tenor.gif?itemid=5705428);
 	}
 });
 
